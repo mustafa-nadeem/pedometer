ff61b60ce1797c8408556673a7413f628460e572
Mustafa Nadeem <mustafanadeem23@gmail.com>
2024-08-13 05:39:30 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/pedomter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pedomter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        useMaterial3: true,
+        primarySwatch: Colors.blue,
+      ),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Pedometer'),),
+    );
+  }
+}
+
+
+
