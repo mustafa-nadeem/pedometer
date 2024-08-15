1e71adf786d460ea1b977ccd3b62950a9d2dffb0
MHBhura <hamzabhura84@gmail.com>
2024-08-15 09:09:49 +0500

    ghjkjkjjk

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Flutter App',
       theme: ThemeData(
         useMaterial3: true,
         primarySwatch: Colors.blue,
